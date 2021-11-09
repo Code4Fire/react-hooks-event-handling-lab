f2293be5d29f69c80f2d3f9bc123608c6784274d
Tiffany_Tam <tamtiffany89@gmail.com>
2021-11-08 22:20:02 -0500

    Completed

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
-// Code EyesOnMe Component Here
+import React, {useState} from "./react";
+
+function EyesOnMe(){
+  function handleFocusEvent (){
+    console.log("Good!")
+  }
+  function handleFocusBlur (){
+    console.log("Hey! Eyes on me!")
+  }
+
+  return(
+    <button onfocus = {handleFocusEvent} onBlur = {handleFocusBlur}>'Eyes on me'</button>
+  );
+}
+
+export default EyesOnMe;
+
+
+
+
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,34 @@
-// Code Keypad Component Here
+import React, {useState} from "./react";
+
+function Keypad(){
+
+  function handleChange () {
+    console.log('Entering password...')
+  }
+  return (
+    <input type="password" onChange= {handleChange}></input>
+  );
+}
+
+export default Keypad;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
